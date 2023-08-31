1dad286185f86800ab9c8cbbd4ed8e0671fcf60a
wangxiaoning <@nPch8vK>
2023-08-31 17:01:00 +0800

    测试1

---
+++ src/main/java/com/progress/progressapi/test/test1.java
@@ -0,0 +1,10 @@
+package com.progress.progressapi.test;
+
+/**
+ * description: test1
+ * date: 2023/8/31 16:54
+ *
+ * @author: wangxiaoning
+ */
+public class test1 {
+}
---
+++ src/main/java/com/progress/progressapi/test/test2.java
@@ -0,0 +1,10 @@
+package com.progress.progressapi.test;
+
+/**
+ * description: test2
+ * date: 2023/8/31 16:54
+ *
+ * @author: wangxiaoning
+ */
+public class test2 {
+}
